751a5602729102e08616b17265c747a946f4391a
YourNoze <57514595+baydim@users.noreply.github.com>
2025-01-16 09:33:50 +0700

    ✨ Initial commit

---
+++ android/app/src/main/kotlin/com/example/test_ximply/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_ximply
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,20 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
+  static const DETAIL_PRODUCT = _Paths.DETAIL_PRODUCT;
+  static const SPLASH = _Paths.SPLASH;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const DETAIL_PRODUCT = '/detail-product';
+  static const SPLASH = '/splash';
+}
